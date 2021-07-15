e8a84271577f8bd536bd2a53bda864e6b84c37a6
sho0terbr <86730475+sho0terbr@users.noreply.github.com>
2021-07-15 17:31:47 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(propriedades) {
-  console.log(propriedades);
   return (
     <Box>
       <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={'https://github.com/${propriedades.githubUser}'} >
+          @{propriedades.githubUser}
+        </a>
+
+      </p>
+      <hr />
+
+
+      <AlurakutProfileSidebarMenuDefault />
+
     </Box>
   )
 }
@@ -34,11 +46,35 @@ export default function Home() {
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box>
             <h1 className="title">
-              Bem vindo(a) 
+              Bem vindo(a)
             </h1>
 
             <OrkutNostalgicIconSet />
           </Box>
+          <Box>
+            <h2>
+              O que você deseja fazer?
+            </h2>
+            <form>
+              <div>
+              <imput 
+              placeholder="Qual vai ser o nome da sua comunidade?" 
+              name="title" 
+              aria-label="Qual vai ser o nome da ua cominidade?" 
+              type= "text"
+              />
+              </div>
+              <div>
+              <imput 
+              placeholder="Coloque uma url para usar de capa"
+              name="imageß" 
+              aria-label="Qual vai ser o nome da ua cominidade?" 
+              
+              />
+              </div>
+            </form>
+          </Box>
+
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
           <ProfileRelationsBoxWrapper>
